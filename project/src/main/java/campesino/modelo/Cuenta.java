7ad8269d01ad2ec8c184c089327f6545af552099
Nazarit0233 <santiagonazarit789@gmail.com>
2025-10-23 18:56:58 -0500

    id Cuenta

--- project/src/main/java/campesino/modelo/Cuenta.java
+++ project/src/main/java/campesino/modelo/Cuenta.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
 import campesino.modelo.Roles.Rol;
 
 public class Cuenta {
+    private int id_Cuenta;
     private Rol rol;
     private String nombre;
     private String correoElectronico;
@@ -12,6 +13,7 @@ public class Cuenta {
     private String telefono;
 
     public Cuenta(Rol rol, String nombre, String correoElectronico, String contraseña, String telefono) {
+        this.id_Cuenta = id_Cuenta;
         this.rol = rol;
         this.nombre = nombre;
         this.correoElectronico = correoElectronico;
@@ -21,7 +23,8 @@ public class Cuenta {
     }
 
     public void mostrarInfoCuenta() {
-        JOptionPane.showMessageDialog(null, "Usuario: " + nombre + 
+        JOptionPane.showMessageDialog(null, "ID cuenta: " + id_Cuenta + 
+            "\nUsuario: " + nombre + 
             "\nRol: " + rol.getNombre() +
             "\nCorreo Electrónico: " + correoElectronico + 
             "\nTeléfono: " + telefono);
